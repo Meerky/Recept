2e97785d48e836aa5faa1fd2de6fc3bd3db273fe
Zsidai Éva <zsidaie@gmail.com>
2023-01-26 20:33:43 +0100

    navbar

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Diab,téné,vega,vegán,normál recept gyüjtemény
 2.ng s -o inditás a böngászőben
 3.npm i futtatása
 4.ng g c components/navbar 
+5. bootstrap behúzása : 
+index.html headerbe : 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+6.bootstrappen kikeresni melyik header kell és bemásolni a nav.html- be
+7. navbar formázása scss/css-ben
+8. 
--- recipe/src/app/components/navbar/navbar.component.ts
+++ recipe/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpService } from '../../service/http.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  
+ 
+  toDisplay: any;
+  recipe: any;
+  recipeForm: FormGroup;
+  id: number = 0;
+  Recipes: Array<any> = [];
+  ingrediens = [];
+  recipes: Array<any> = [];
+  chosenRecipe: Array<any> = [];
+
+  constructor(
+    // private http: HttpService,
+    private router: Router
+  ) {
+    this.recipeForm = new FormGroup({
+      name: new FormControl(''),
+      imgUrl: new FormControl(''),
+      type: new FormControl(''),
+      ingrediens: new FormControl(''),
+      selectedList: new FormArray([]),
+      preparation: new FormControl('')
+
+    })
+  }
+type:any;
 
   ngOnInit(): void {
+    // this.getRecipe();
+
   }
+
+  chooseRecipe = (type: any) => {
+    // this.http.chosenRecipe.next(type);
+    this.router.navigate(['try-card',type]);
+    // this.getRecipe()
+
+  }
+
+
+  selectedList: Array<any> = [];
+
+  showByCategory(category:any,recipeType:any) {
+    this.toDisplay = category?category:recipeType;
+    this.recipes = this.Recipes.filter(recipe => recipe.selectedList.indexOf(category) != -1 ||  recipe.type === recipeType);
+    this.toDisplay;
+    // this.getRecipe();
+  }
+
+  // getRecipe() {
+  //   this.http.getRecipeList().subscribe(
+  //     (recipe: any) => this.Recipes = recipe,
+
+  //     (err) => console.log(err)
+  //   )
+
+  // }
+ 
 
 }
